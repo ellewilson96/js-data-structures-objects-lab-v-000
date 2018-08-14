dcd310cee534a2ea97d2ddf38f90d48b2487a308
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-14 14:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ function deleteFromDriverByKey(driver, key) {
   const newObj = Object.assign({}, driver);
 
   delete newObj[key];
-  
+
   return newObj;
 }
 
-function destructivelyDeleteFromDriverByKey() {
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
 
+  return driver;
 }
